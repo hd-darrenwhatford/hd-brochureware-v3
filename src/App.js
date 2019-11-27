179db8557c6c873876dd7644eee6bd1d33add486
Dan Jackson <d.jackson.contact@gmail.com>
2019-11-27 08:00:50 +0000

    Feature/react button component (#4)
    * React Button component added
    
    * Button component rendered within the app
    
    * React type prop added to the Button component
    
    * Default type prop value added to the Button component
    
    * Car value passed into children prop of the Button component
    
    * App component created
    
    * Update to the index.js file
    
    * React Fragment applied to wrap around multiple Button components within the App component
    
    * Newline added to the end of the App and Button component file
    
    * .gitignore updated to avoid the stats.json file being committed to version control
    
    * Comment added to the .gitignore file

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Button from './Button'
+
+const App = () => {
+    return (
+    	<React.Fragment>
+	    	<Button>Car</Button>
+	    	<Button>Van</Button>
+    	</React.Fragment>
+    )
+}
+
+export default App
---
+++ src/Button.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = ({ type = 'button', children }) => {
+    return (
+        <button type={type}>{children}</button>
+    )
+}
+
+export default Button
