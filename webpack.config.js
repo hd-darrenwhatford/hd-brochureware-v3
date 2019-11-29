c6f65dc1053b0265d74a7af1bfb04ccf2227c779
Dan Jackson <danjackson@hastingsdirect.com>
2019-11-29 16:17:59 +0000

    Ternary added to create a minified and unminified bundle when the production or development build script is run

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const env = process.env.NODE_ENV
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
@@ -20,5 +21,9 @@ module.exports = {
                 source: false
             }
         })
-    ]
+    ],
+    output: {
+        // Set filename of bundle if production or development mode is used
+        filename: env === 'production' ? 'main.bundle.min.js' : 'main.bundle.js'
+    }
 }
