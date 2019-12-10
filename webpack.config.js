4e3e3e5508a89931b4b5f94b870dfd71f65adc6f
Dan Jackson <danjackson@hastingsdirect.com>
2019-12-10 17:47:54 +0000

    devServer open property set to true to open the browser window/tab when the devServer runs

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,7 @@ module.exports = {
     devtool: 'source-map',
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
+        open: true,
         compress: true,
         port: 9000
     },
