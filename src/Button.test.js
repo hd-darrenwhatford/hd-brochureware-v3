25413079a348ef5c6c7a32780db645380e763626
Dan Jackson <danjackson@hastingsdirect.com>
2019-12-11 22:39:54 +0000

    Test file written for the Button component

---
+++ src/Button.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Button from './Button'
+
+describe('Button', () => {
+    it('Button renders correctly', () => {
+        const wrapper = shallow(<Button />)
+    })
+})
+    
